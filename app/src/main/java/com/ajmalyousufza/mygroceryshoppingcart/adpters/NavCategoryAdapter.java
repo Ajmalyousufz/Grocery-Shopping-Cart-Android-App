d87b9f6779855315fb1c7922dd6d3aaef0662950
Ajmalyousufza <ajmalyousufza@gmail.com>
2022-01-05 19:29:34 +0530

    Profile fragment and Categories Fragment Item design and Dummy item fetching from firebase Completed

---
+++ app/src/main/java/com/ajmalyousufza/mygroceryshoppingcart/adpters/NavCategoryAdapter.java
@@ -0,0 +1,62 @@
+package com.ajmalyousufza.mygroceryshoppingcart.adpters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ajmalyousufza.mygroceryshoppingcart.R;
+import com.ajmalyousufza.mygroceryshoppingcart.models.NavCategoryModel;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class NavCategoryAdapter extends RecyclerView.Adapter<NavCategoryAdapter.ViewHolder> {
+
+     Context context;
+     List<NavCategoryModel> navCategoryModelList;
+
+    public NavCategoryAdapter(Context context, List<NavCategoryModel> navCategoryModelList) {
+        this.context = context;
+        this.navCategoryModelList = navCategoryModelList;
+    }
+
+    @NonNull
+      @Override
+      public NavCategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+          return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.nav_category_item,parent,false));
+      }
+
+      @Override
+      public void onBindViewHolder(@NonNull NavCategoryAdapter.ViewHolder holder, int position) {
+
+          Glide.with(context).load(navCategoryModelList.get(position).getImage_url()).into(holder.nav_cat_img);
+          holder.name.setText(navCategoryModelList.get(position).getName());
+          holder.description.setText(navCategoryModelList.get(position).getDescription());
+          holder.discount.setText(navCategoryModelList.get(position).getDiscount());
+
+      }
+
+      @Override
+      public int getItemCount() {
+          return navCategoryModelList.size();
+      }
+
+      public class ViewHolder extends RecyclerView.ViewHolder {
+          ImageView nav_cat_img;
+          TextView name,description,discount;
+          public ViewHolder(@NonNull View itemView) {
+              super(itemView);
+
+              nav_cat_img = itemView.findViewById(R.id.nav_catogory_item_image);
+              name = itemView.findViewById(R.id.nav_category_name);
+              description = itemView.findViewById(R.id.nav_category_description);
+              discount = itemView.findViewById(R.id.nav_category_discountrate);
+          }
+      }
+  }
--- app/src/main/java/com/ajmalyousufza/mygroceryshoppingcart/ui/Category/CategoryFragment.java
+++ app/src/main/java/com/ajmalyousufza/mygroceryshoppingcart/ui/Category/CategoryFragment.java
@@ -4,20 +4,67 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ajmalyousufza.mygroceryshoppingcart.R;
+import com.ajmalyousufza.mygroceryshoppingcart.adpters.NavCategoryAdapter;
+import com.ajmalyousufza.mygroceryshoppingcart.adpters.PopularAdatper;
+import com.ajmalyousufza.mygroceryshoppingcart.models.NavCategoryModel;
+import com.ajmalyousufza.mygroceryshoppingcart.models.PopularModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
 
+    List<NavCategoryModel> navCategoryModelList;
+    RecyclerView recyclerView;
+    NavCategoryAdapter navCategoryAdapter;
+    FirebaseFirestore db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
 
       View root = inflater.inflate(R.layout.fragment_category,container,false);
+
+      db = FirebaseFirestore.getInstance();
+      recyclerView = root.findViewById(R.id.category_recycler_id);
+
+        //Nav Category items
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        navCategoryModelList = new ArrayList<>();
+        navCategoryAdapter = new NavCategoryAdapter(getActivity(),navCategoryModelList);
+        recyclerView.setAdapter(navCategoryAdapter);
+
+        db.collection("NavCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                NavCategoryModel navCategoryModel = document.toObject(NavCategoryModel.class);
+                                navCategoryModelList.add(navCategoryModel);
+                                navCategoryAdapter.notifyDataSetChanged();
+                                //progressBar.setVisibility(View.GONE);
+                                //scrollView.setVisibility(View.VISIBLE);
+                                //Toast.makeText(getActivity(), "Successfull.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error getting documents."+task.getException(), Toast.LENGTH_SHORT).show();
+                        }}});
 
         return root;
     }
